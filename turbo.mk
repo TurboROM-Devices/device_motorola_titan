ac398a63f14a95c9ac14c93d2925eb5d018136d7
Raleigh Matlock <raleighman2@gmail.com>
2016-04-16 12:50:30 -0400

    Turbo-ify

--- turbo.mk
+++ turbo.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Turbo stuff.
+$(call inherit-product, vendor/turbo/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := cm_titan
+PRODUCT_NAME := turbo_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
